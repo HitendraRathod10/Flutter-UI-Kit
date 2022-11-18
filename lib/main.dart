fcfa67444afa8d8dd907aa76afe4f169e3f1aa21
Hitendra <hitendrasinhrathod.elsner@gmail.com>
2022-11-18 19:25:43 +0530

    responsive plug-in

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_kit/Splash/splash_screen.dart';
-
+import 'package:responsive_sizer/responsive_sizer.dart';
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 void main() {
   runApp(const MyApp());
+  HttpOverrides.global = MyHttpOverrides();
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +20,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const SplashScreen(),
+    return ResponsiveSizer(
+      builder: (BuildContext , Orientation , ScreenType ) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+          ),
+          home: const SplashScreen(),
+        );
+      },maxTabletWidth: 900,
     );
   }
 }
