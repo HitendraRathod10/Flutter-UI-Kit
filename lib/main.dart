066a9f6614e053a00b3f2f7a37dc407a66abcf80
Jigar <jigargodasara@elsner.com>
2023-09-15 13:10:28 +0530

    branch

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyHttpOverrides extends HttpOverrides {
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
       ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+                (X509Certificate cert, String host, int port) => true;
   }
 }
 void main() {
